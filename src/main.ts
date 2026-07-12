1e6e28f837012ec063df82a6babd9aa0385dbd36
Azaameth <maxime.boches@gmail.com>
2026-07-12 12:05:45 +0200

    debug du flash au chargement du html

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+
+// Révèle le contenu une fois monté
+const el = document.getElementById('app')
+if (el) el.style.visibility = ''
